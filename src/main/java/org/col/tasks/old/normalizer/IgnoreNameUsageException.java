e33bc2931962ecf8c21ef85a86aeab2f3a81e03a
mdoering <mdoering@gbif.org>
2017-09-05 17:24:37 +0200

    Remove old clb classes

--- src/main/java/org/col/tasks/old/normalizer/IgnoreNameUsageException.java
+++
@@ -1,8 +0,0 @@
-package org.col.tasks.old.normalizer;
-
-public class IgnoreNameUsageException extends Exception {
-
-  public IgnoreNameUsageException(String message) {
-    super(message);
-  }
-}
--- src/main/java/org/col/tasks/old/normalizer/NormalizerConstants.java
+++
@@ -1,9 +0,0 @@
-package org.col.tasks.old.normalizer;
-
-/**
- *
- */
-public class NormalizerConstants {
-
-  public static final String PLACEHOLDER_NAME = "Incertae sedis";
-}
